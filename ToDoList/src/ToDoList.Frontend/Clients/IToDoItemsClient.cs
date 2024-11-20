7fb108460093ca808e9110da5106fbda0a6c4acf
Martin Černil <m.cernil@gmail.com>
2024-11-20 22:44:42 +0100

    ToDoItemsClient added all other methods

--- ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -3,5 +3,9 @@ using ToDoList.Frontend.Views;
 
 public interface IToDoItemsClient
 {
+    public Task CreateItemAsync(ToDoItemView itemView);
+    public Task<ToDoItemView?> ReadItemByIdAsync(int itemId);
     public Task<List<ToDoItemView>> ReadItemsAsync();
+    public Task DeleteItemAsync(ToDoItemView itemView);
+    public Task UpdateItemAsync(ToDoItemView itemView);
 }
