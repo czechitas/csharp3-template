bcfe031a76b693fbb613a71c35da5d6c10664285
Pluchova <nikola.pluchova@seznam.cz>
2024-11-21 10:01:49 +0100

    po lekci 9

---
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -0,0 +1,6 @@
+namespace ToDoList.Frontend.Clients;
+using ToDoList.Frontend.Views;
+public interface IToDoItemsClient
+{
+    public Task<List<ToDoItemView>> ReadItemsAsync();
+}
---
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -0,0 +1,19 @@
+namespace ToDoList.Frontend.Clients;
+using ToDoList.Domain.DTOs;
+using ToDoList.Frontend.Views;
+public class ToDoItemsClient(HttpClient httpClient) : IToDoItemsClient
+{
+    public async Task<List<ToDoItemView>> ReadItemsAsync()
+    {
+        var toDoItemsView = new List<ToDoItemView>();
+        var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems");
+        toDoItemsView = response.Select(dto => new ToDoItemView
+        {
+            ToDoItemId = dto.Id,
+            Name = dto.Name,
+            Description = dto.Description,
+            IsCompleted = dto.IsCompleted
+        }).ToList();
+        return toDoItemsView;
+    }
+}
