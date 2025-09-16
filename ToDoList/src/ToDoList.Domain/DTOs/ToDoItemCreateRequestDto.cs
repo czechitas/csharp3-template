1169d180678f8c08c8b7d446f8ce561803b9daa9
Martin Černil <m.cernil@gmail.com>
2025-09-16 18:04:34 +0200

    delete source code

--- ToDoList/src/ToDoList.Domain/DTOs/ToDoItemCreateRequestDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ToDoList.Domain.DTOs;
-
-using ToDoList.Domain.Models;
-
-public record ToDoItemCreateRequestDto(string Name, string Description, bool IsCompleted) //id is generated
-{
-    public ToDoItem ToDomain() => new() { Name = Name, Description = Description, IsCompleted = IsCompleted };
-}
--- ToDoList/src/ToDoList.Domain/DTOs/ToDoItemUpdateRequestDto.cs
+++
@@ -1,7 +0,0 @@
-namespace ToDoList.Domain.DTOs;
-using ToDoList.Domain.Models;
-
-public record class ToDoItemUpdateRequestDto(string Name, string Description, bool IsCompleted)
-{
-    public ToDoItem ToDomain() => new() { Name = this.Name, Description = this.Description, IsCompleted = this.IsCompleted };
-}
