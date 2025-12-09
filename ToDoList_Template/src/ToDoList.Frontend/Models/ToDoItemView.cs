83eb72d828336767459d41726b0c4c62fa2c7041
Martin Černil <martin.cernil@thermofisher.com>
2025-12-09 18:54:13 +0100

    Prvni cast lekce 11

--- ToDoList_Template/src/ToDoList.Frontend/Models/ToDoItemView.cs
+++ ToDoList_Template/src/ToDoList.Frontend/Models/ToDoItemView.cs
@@ -1,9 +1,14 @@
 namespace ToDoList.Frontend.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class ToDoItemView
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Name is mandatory.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Description is mandatory.")]
+    [StringLength(250)]
     public string Description { get; set; }
     public bool IsCompleted { get; set; }
 }
--- ToDoList_Template/src/ToDoList.Frontend/Program.cs
+++ ToDoList_Template/src/ToDoList.Frontend/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ToDoItemApiAddress"]) });
 builder.Services.AddScoped<IToDoItemsClient, ToDoItemsClient>();
 
 var app = builder.Build();
